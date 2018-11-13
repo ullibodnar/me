6ec08f32d52946a2b3d971d99bae02f50bac36da
ullibodnar <ullikins@gmail.com>
2018-11-13 16:20:27 +1300

    Refactor and redesign everything

--- public/js/index.ts
+++ public/js/index.ts
@@ -1,3 +1,5 @@
+import Typed from 'typed.js'
+
 const navToggleMobile: HTMLElement = document.querySelector(".nav-toggle")
 const header: HTMLElement = document.querySelector("header")
 
@@ -7,3 +9,14 @@ function toggleMobileNav() {
   navToggleMobile.classList.toggle("x")
   header.classList.toggle("toggled")
 }
+
+var options2 = {
+  strings: ["", "<p>Hi! I<p><h2>build things</h2>", "<p>Hi! I</p><h2>write code</h2>", "<p>Hi! I am a</p><h2>developer</h2>", "<p>Hi! I'm</p><h2>Ulli Bodnar</h2>"],
+  typeSpeed: 50,
+  smartBackspace: true,
+  startDelay: 1000,
+  backSpeed: 20,
+  showCursor: false
+}
+
+var typed = new Typed(".typed-content", options2);
