df537b73b7189a45d6b7ba6007e202a282093399
ullibodnar <ullikins@gmail.com>
2018-11-13 19:40:46 +1300

    Make particles create a random effect

---
+++ public/js/randomEffect.js
@@ -0,0 +1,122 @@
+function flipCoin () {
+  const number = Math.floor(Math.random() * 2)
+
+  return number !== 0
+}
+
+function chooseValue (min, max, whole) {
+  const number = Math.random() * (max + min) + min
+
+  return whole ? Math.floor(number) : number
+}
+
+particlesJS('particles', {
+  particles: {
+    number: {
+      value: chooseValue(100, 300, true),
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    },
+    color: {
+      value: '#ffffff'
+    },
+    shape: {
+      type: 'circle',
+      stroke: {
+        width: 0,
+        color: '#000000'
+      },
+      polygon: {
+        nb_sides: 5
+      },
+      image: {
+        src: 'img/github.svg',
+        width: 100,
+        height: 100
+      }
+    },
+    opacity: {
+      value: 1,
+      random: true,
+      anim: {
+        enable: true,
+        speed: 1,
+        opacity_min: 0,
+        sync: false
+      }
+    },
+    size: {
+      value: chooseValue(1, 2.5),
+      random: true,
+      anim: {
+        enable: false,
+        speed: 4,
+        size_min: 0.3,
+        sync: false
+      }
+    },
+    line_linked: {
+      enable: flipCoin(),
+      distance: 70,
+      color: '#ffffff',
+      opacity: 0.2,
+      width: 1
+    },
+    move: {
+      enable: true,
+      speed: chooseValue(0.5, 2),
+      direction: 'none',
+      random: true,
+      straight: false,
+      out_mode: 'out',
+      bounce: false,
+      attract: {
+        enable: false,
+        rotateX: 600,
+        rotateY: 600
+      }
+    }
+  },
+  interactivity: {
+    detect_on: 'canvas',
+    events: {
+      onhover: {
+        enable: false,
+        mode: 'repulse'
+      },
+      onclick: {
+        enable: false,
+        mode: 'repulse'
+      },
+      resize: true
+    },
+    modes: {
+      grab: {
+        distance: 400,
+        line_linked: {
+          opacity: 1
+        }
+      },
+      bubble: {
+        distance: 250,
+        size: 0,
+        duration: 2,
+        opacity: 0,
+        speed: 3
+      },
+      repulse: {
+        distance: 70,
+        duration: 0.1
+      },
+      push: {
+        particles_nb: 4
+      },
+      remove: {
+        particles_nb: 2
+      }
+    }
+  },
+  retina_detect: true
+})
